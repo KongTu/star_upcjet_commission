cd31f2b2e46bcd816cd5fb8832437dd3ca172745
Zhoudunming Tu <zhoudunmingtu@lpo-169579.mynetworksettings.com>
2023-06-01 17:38:45 -0400

    initial commit

--- subMudst.sh
+++ subMudst.sh
@@ -5,7 +5,7 @@ dir=`pwd`
 filelist=$dir/$input
 scriptdir=`pwd`/scripts
 mkdir -p $scriptdir
-outdir=/star/u/starkong/trigger-testing/jobtest/batch_3
+outdir=/star/u/starkong/gpfs01/star_upcjet_commission/output/batch_1
 echo $filelist
 mkdir -p $outdir
 mkdir -p $outdir/log
