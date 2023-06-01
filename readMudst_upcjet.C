d51ca37a7273fbaf18abe77b9d05ddbecaf59153
Zhoudunming Tu <zhoudunmingtu@lpo-169579.mynetworksettings.com>
2023-06-01 16:59:39 -0400

    initial commit

--- readMudst_upcjet.C
+++ readMudst_upcjet.C
@@ -105,113 +105,113 @@ void readMudst_upcjet(
     mMuDst = muDstMaker->muDst();
     StMuEvent *mMuEvent = mMuDst->event();
     //loop over triggers
-    int trig_index=-1;
-    for(int i=5;i<6;i++){
-      if( mMuEvent->triggerIdCollection().nominal().isTrigger(commTriggerID[i]) ) trig_index=i;
-    }
-    if(trig_index<0) continue; //protection
-
-    /************ZDC ADC***************/ 
-    /******east = 0, west =1**********/
-    int mNZdcPmt = 3;
-    double zdcadc_e = 0;
-    double zdcadc_w = 0;
-    for (Int_t ipmt=1; ipmt<=mNZdcPmt; ipmt++) {
-        zdcadc_e = zdcadc_e + trgdata->zdcADC(0, ipmt); 
-        zdcadc_w = zdcadc_w + trgdata->zdcADC(1, ipmt);
-    }
-    // ZDC reqirement 1nXn
-    int ZDCW=0;
-    int ZDCE=0;
-    if(zdcadc_w >= 350){ZDCW=1;}
-    if(zdcadc_e >= 350){ZDCE=1;}
-
-    //1) now decide direction based on BBC first.
-    int direction_index=-1;
-    if(BBCE && !BBCW) direction_index=0;
-    if(!BBCE && BBCW) direction_index=1;
-    if(direction_index<0) continue; //protection
-
-    //2) filling ZDC distributions based on BBC configurations
-    zdcadc_east[trig_index][direction_index]->Fill(zdcadc_e);
-    zdcadc_west[trig_index][direction_index]->Fill(zdcadc_w);
-    zdcadc[trig_index][direction_index]->Fill(zdcadc_e,zdcadc_w);
     
-    //3) now add ZDC to BBC requirements
-    direction_index=-1;
-    if(BBCE && !BBCW && ZDCE && !ZDCW) direction_index=0;
-    if(!BBCE && BBCW && !ZDCE && ZDCW) direction_index=1;
-    if(direction_index<0) continue; //protection again
-   
-  /*********** EEMC ************/
-    int eemc_adc = 0;
-    //cout<<muemc->patchEndcap()<<endl;
-    for (Int_t ipatch=0; ipatch<90; ipatch++){
-	     eemc_adc = eemc_adc + trgdata->eemcJetPatch(ipatch);
+    for(int i=0;i<num_trgs;i++){
+      int trig_index=-1;
+      if( mMuEvent->triggerIdCollection().nominal().isTrigger(commTriggerID[i]) ) trig_index=i;
+      if(trig_index<0) continue; //protection
+
+      /************ZDC ADC***************/ 
+      /******east = 0, west =1**********/
+      int mNZdcPmt = 3;
+      double zdcadc_e = 0;
+      double zdcadc_w = 0;
+      for (Int_t ipmt=1; ipmt<=mNZdcPmt; ipmt++) {
+          zdcadc_e = zdcadc_e + trgdata->zdcADC(0, ipmt); 
+          zdcadc_w = zdcadc_w + trgdata->zdcADC(1, ipmt);
+      }
+      // ZDC reqirement 1nXn
+      int ZDCW=0;
+      int ZDCE=0;
+      if(zdcadc_w >= 350){ZDCW=1;}
+      if(zdcadc_e >= 350){ZDCE=1;}
+
+      //1) now decide direction based on BBC first.
+      int direction_index=-1;
+      if(BBCE && !BBCW) direction_index=0;
+      if(!BBCE && BBCW) direction_index=1;
+      if(direction_index<0) continue; //protection
+
+      //2) filling ZDC distributions based on BBC configurations
+      zdcadc_east[trig_index][direction_index]->Fill(zdcadc_e);
+      zdcadc_west[trig_index][direction_index]->Fill(zdcadc_w);
+      zdcadc[trig_index][direction_index]->Fill(zdcadc_e,zdcadc_w);
+      
+      //3) now add ZDC to BBC requirements
+      direction_index=-1;
+      if(BBCE && !BBCW && ZDCE && !ZDCW) direction_index=0;
+      if(!BBCE && BBCW && !ZDCE && ZDCW) direction_index=1;
+      if(direction_index<0) continue; //protection again
+     
+    /*********** EEMC ************/
+      int eemc_adc = 0;
+      //cout<<muemc->patchEndcap()<<endl;
+      for (Int_t ipatch=0; ipatch<90; ipatch++){
+  	     eemc_adc = eemc_adc + trgdata->eemcJetPatch(ipatch);
+      }
+      eemc[trig_index][direction_index]->Fill(eemc_adc);
+
+    /*************BBC*************/
+      double bbcadc_e = 0;
+      double bbcadc_w = 0;
+      for (Int_t ipmt=1; ipmt<=16; ipmt++) {
+  	      bbcadc_e = bbcadc_e + trgdata->bbcADC(0, ipmt);	
+          bbcadc_w = bbcadc_w + trgdata->bbcADC(1, ipmt);
+      }
+      bbcadc[trig_index][direction_index]->Fill(bbcadc_e, bbcadc_w);
+
+    /*************VPD*************/
+      double vpdadc_e = 0;
+      double vpdadc_w = 0;
+      for (Int_t ipmt=1; ipmt<=16; ipmt++) {
+      	vpdadc_e = vpdadc_e + trgdata->vpdADC(0, ipmt);	
+        vpdadc_w = vpdadc_w + trgdata->vpdADC(1, ipmt);
+      }
+      vpdadc[trig_index][direction_index]->Fill(vpdadc_e, vpdadc_w);
+
+    /*********EPD ************/
+      int nhit = StMuDst::numberOfEpdHit();
+      //cout<<"nhit"<<nhit<<endl;
+      double epdadc_e = 0;
+      double epdadc_w = 0;
+      for(int iEpdHit = 0; iEpdHit < nhit; iEpdHit++){
+        const StMuEpdHit* muEpdHit = StMuDst::epdHit(iEpdHit);
+        int adc = muEpdHit->adc();
+        if( muEpdHit->side()<0) epdadc_e += adc;
+        else epdadc_w += adc;
+
+        TVector3 StraightLine = mEpdGeom->RandomPointOnTile(muEpdHit->id()) - pRcVx;
+        double phi_epd = StraightLine.Phi();
+        double eta_epd = StraightLine.Eta();
+        //cout<<"phi_epd "<<phi_epd<<" eta_epd "<<eta_epd <<endl;
+        epdeta[trig_index][direction_index]->Fill(eta_epd);
+      }
+      epdadc[trig_index][direction_index]->Fill(epdadc_e, epdadc_w);
+
+      float ranking = vertex->ranking();
+      int nutrks = vertex->nTracksUsed();
+      int notrks = vertex->noTracks();
+      int nprim = StMuDst::numberOfPrimaryTracks();
+      int nglob = StMuDst::numberOfGlobalTracks();
+      //Printf("ranking=%lg nutrks=%d nprim=%d notrks=%d nglob=%d\n", ranking, nutrks, nprim, notrks, nglob);
+
+      int numtrk = 0;
+      for(int iTrack = 0; iTrack < nprim; iTrack++){
+        const StMuTrack* muTrack = StMuDst::primaryTracks(iTrack);
+        int id = muTrack->id();
+        int nhits = muTrack->nHits();
+        int nhitsfit = muTrack->nHitsFit();
+        int nhitsposs = muTrack->nHitsPoss();
+        bool type = muTrack->type();
+        bool flag = muTrack->flag();
+        float pt = muTrack->pt();
+        if(pt<0.2)continue;
+        double eta = muTrack->eta();
+        htrketa[trig_index][direction_index]->Fill(eta);
+        numtrk++;
+      }
+      hntrk[trig_index][direction_index]->Fill(numtrk);
     }
-    eemc[trig_index][direction_index]->Fill(eemc_adc);
-
-  /*************BBC*************/
-    double bbcadc_e = 0;
-    double bbcadc_w = 0;
-    for (Int_t ipmt=1; ipmt<=16; ipmt++) {
-	      bbcadc_e = bbcadc_e + trgdata->bbcADC(0, ipmt);	
-        bbcadc_w = bbcadc_w + trgdata->bbcADC(1, ipmt);
-    }
-    bbcadc[trig_index][direction_index]->Fill(bbcadc_e, bbcadc_w);
-
-  /*************VPD*************/
-    double vpdadc_e = 0;
-    double vpdadc_w = 0;
-    for (Int_t ipmt=1; ipmt<=16; ipmt++) {
-    	vpdadc_e = vpdadc_e + trgdata->vpdADC(0, ipmt);	
-      vpdadc_w = vpdadc_w + trgdata->vpdADC(1, ipmt);
-    }
-    vpdadc[trig_index][direction_index]->Fill(vpdadc_e, vpdadc_w);
-
-  /*********EPD ************/
-    int nhit = StMuDst::numberOfEpdHit();
-    //cout<<"nhit"<<nhit<<endl;
-    double epdadc_e = 0;
-    double epdadc_w = 0;
-    for(int iEpdHit = 0; iEpdHit < nhit; iEpdHit++){
-      const StMuEpdHit* muEpdHit = StMuDst::epdHit(iEpdHit);
-      int adc = muEpdHit->adc();
-      if( muEpdHit->side()<0) epdadc_e += adc;
-      else epdadc_w += adc;
-
-      TVector3 StraightLine = mEpdGeom->RandomPointOnTile(muEpdHit->id()) - pRcVx;
-      double phi_epd = StraightLine.Phi();
-      double eta_epd = StraightLine.Eta();
-      //cout<<"phi_epd "<<phi_epd<<" eta_epd "<<eta_epd <<endl;
-      epdeta[trig_index][direction_index]->Fill(eta_epd);
-    }
-    epdadc[trig_index][direction_index]->Fill(epdadc_e, epdadc_w);
-
-    float ranking = vertex->ranking();
-    int nutrks = vertex->nTracksUsed();
-    int notrks = vertex->noTracks();
-    int nprim = StMuDst::numberOfPrimaryTracks();
-    int nglob = StMuDst::numberOfGlobalTracks();
-    //Printf("ranking=%lg nutrks=%d nprim=%d notrks=%d nglob=%d\n", ranking, nutrks, nprim, notrks, nglob);
-
-    int numtrk = 0;
-    for(int iTrack = 0; iTrack < nprim; iTrack++){
-      const StMuTrack* muTrack = StMuDst::primaryTracks(iTrack);
-      int id = muTrack->id();
-      int nhits = muTrack->nHits();
-      int nhitsfit = muTrack->nHitsFit();
-      int nhitsposs = muTrack->nHitsPoss();
-      bool type = muTrack->type();
-      bool flag = muTrack->flag();
-      float pt = muTrack->pt();
-      if(pt<0.2)continue;
-      double eta = muTrack->eta();
-      htrketa[trig_index][direction_index]->Fill(eta);
-      numtrk++;
-    }
-    hntrk[trig_index][direction_index]->Fill(numtrk);
-
   }
 
   fout->Write();
