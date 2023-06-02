e9e1bf4edce6d7b7093b8ee32191fb7e35c43d83
Zhoudunming Tu <zhoudunmingtu@lpo-169579.mynetworksettings.com>
2023-06-01 22:55:45 -0400

    change

--- readMudst_upcjet.C
+++ readMudst_upcjet.C
@@ -1,6 +1,6 @@
 #include "TLorentzVector.h"
 #include <vector>
-const unsigned long nevents = 400;
+const unsigned long nevents = 40000000;
 int commTriggerID[]={2,3,16,17,18,19,20};
 int prodTriggerID[]={900501,900502,900503,900504,900505,900506,900507};
 
